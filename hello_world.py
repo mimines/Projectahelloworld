eb6e1f4a561fbe861042dd69d6b612ad7237a499
mimines <46932473+mimines@users.noreply.github.com>
2020-07-20 17:13:48 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 def say_hello():
 	print('Hello World')
 	print('Hello world from python 3 this is the changes')
-	print('hello there')
+	print('hello there how are you doing?')
 	for i in range(3):
 		print('Hi!')
 	
